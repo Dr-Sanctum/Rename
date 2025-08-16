5c57e5def131c26464b82014e8e198b2257537d9
anton <Di_oXid@mail.ru>
2025-08-16 19:26:55 +0500

    first commit

---
+++ Rename/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myPath = Directory.GetCurrentDirectory();
+        string[] arrPath = Directory.GetFiles(myPath);
+        
+
+        for (int i = 0; i < arrPath.Length; i++)
+        {
+            Console.WriteLine(arrPath[i]);
+        }
+        Microsoft.VisualBasic.FileIO.FileSystem.RenameFile("111.txt","222.txt");
+
+        arrPath = Directory.GetFiles(myPath);
+
+        for (int i = 0; i < arrPath.Length; i++)
+        {
+            Console.WriteLine(arrPath[i]);
+        }
+    }
+}
